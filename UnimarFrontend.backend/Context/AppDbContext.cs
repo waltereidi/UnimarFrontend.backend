4e373ca3b5c756ed7c7f867fd4b4b5bbf2c9f18a
walter <walter-eidi@hotmail.com>
2025-12-06 21:28:01 -0300

    Adicionar arquivos de projeto.

---
+++ UnimarFrontend.backend/Context/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UnimarFrontend.backend.Models;
+
+namespace UnimarFrontend.backend.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+
+    }
+}
---
+++ UnimarFrontend.backend/Models/Book.cs
@@ -0,0 +1,12 @@
+﻿namespace UnimarFrontend.backend.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string ISBN { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string ThumNail { get; set; }
+    }
+}
