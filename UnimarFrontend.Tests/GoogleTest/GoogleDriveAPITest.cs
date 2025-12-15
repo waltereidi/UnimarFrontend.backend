affe190e223c8dc4fed963015d330b2c2d11e929
walter <walter-eidi@hotmail.com>
2025-12-14 23:35:30 -0300

    c

---
+++ UnimarFrontend.Tests/GoogleTest/GoogleDriveAPITest.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnimarFrontend.GoogleDrive.GoogleDrive.Api;
+using UnimarFrontend.GoogleDrive.Interfaces;
+
+namespace UnimarFrontend.Tests.GoogleTest
+{
+    public class GoogleDriveAPITest : Configuration
+    {
+        private readonly GoogleDriveRead _serviceRead;
+        public GoogleDriveAPITest() : base("UnimarFrontEnd.GoogleDrive", "Configuration")
+        {
+            _serviceRead = new GoogleDriveRead();
+        }
+        [Fact]
+        public void TesteListar()
+        {
+            var result = _serviceRead.GetDriveFilesFromCreationDate(DateTime.Now.AddDays(-20) , DateTime.Now);
+            var result2 = _serviceRead.GetDriveFiles();
+            var result3 = _serviceRead.GetDriveFiles();
+            var result4 = _serviceRead.GetDriveFiles();
+
+            Assert.NotNull(result);
+        }
+        [Fact]
+        public void TestDownload()
+        {
+
+            var result = _serviceRead.GetDriveFiles();
+            var file = result.First();
+
+            IGoogleDriveDownloadStrategy downloadService = new GoogleDriveDownload(file, base._fileOutputDir);
+
+
+            var fi = downloadService.Start();
+            Assert.True(fi.Exists);
+        }
+
+    }
+}
